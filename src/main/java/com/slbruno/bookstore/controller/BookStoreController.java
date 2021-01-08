02b1d72a48d093821b83e89a71aa85e92473047e
sergio-bruno <sergio.bruno.ti@gmail.com>
2021-01-08 12:22:55 -0300

    implementando o swagger

--- src/main/java/com/slbruno/bookstore/controller/BookStoreController.java
+++ src/main/java/com/slbruno/bookstore/controller/BookStoreController.java
@@ -4,10 +4,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
 @RestController
 @RequestMapping("/api/hw/books")	
 public class BookStoreController {
 	
+	@ApiOperation(value = "Api rest para teste inicial")
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message = "Sucess method return")
+	})
 	@GetMapping
 	public String hello() {
 		return "Hello world!";
